188a29de7d71518bcc68d0ca7eddf4ecbfd56396
Slayerzzz <killerzzzzboi@gmail.com>
2022-05-23 14:41:32 +0200

    inlogpagina

--- inloggen.php
+++ inloggen.php
@@ -4,17 +4,17 @@ if (isset($_POST['submit'])) {
     if (!empty($_POST['username']) && !empty($_POST['password'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $user = 'admin';
-        $pass = 'admin';
+        $user = 'loempot';
+        $pass = 'Loempot';
         if ($username == $user && $password == $pass) {
             session_start();
             $_SESSION['ingelogd'] = true;
-            header("Location: ingelogd.php");
+            header("Location: index.php");
         }else{
-        $error = "Username of password is verkeerd!";  
+        $error = "U heeft de verkeerde gegevens ingevuld!";  
         }
     } else {
-        $error = "Username & Password zijn verplicht!";
+        $error = "U heeft niet alles ingevuld!";
     }
 }
 
@@ -26,15 +26,51 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Login</title>
 </head>
 <body>
-    <h1>Login met uw gegevens:</h1>
+<header>
+<a href="index.php"><img src="images/logopoke.png" alt="logopoke" height="200px"></a>
+<li>
+    <a href="index.php">Homepage</a>
+    <a href="Info.php">Info</a>
+    <a href="Webpage.php">Webpage</a>
+    <a href="inloggen.php">Inloggen</a>
+</li>
+
+</header>
+
+    <article class="inlog">
+    <h1 class="tekst">Log hier in met uw gegevens:</h1>
     <form method="POST">
-        <?php echo $error; ?> <br>
+        <?php echo $error; ?> 
         <label>Username</label><br><input type="text" name="username" /><br>
         <label>Password</label><br><input type="password" name="password" /><br><br>
-        <input type="submit" name="submit" value="Inloggen" />
-</form>
+        <input class="inloggen" type="submit" name="submit" value="Inloggen" />
+    </form>
+    </article>
+        
+
+<footer>
+        <h3 class="adres">Gegevens:</h3>
+        <h5 class="adres">🏡: Bètaplein 18 2321 KS Leiden</h5>
+        <h5 class="adres">📧: info@mborijnland.nl</h5>
+        <h5 class="adres">📞: 088 222 17 77</h5>
+        <h3 class="claimer">© B Loempot</h3>
+
+
+        <article class="contact">
+        <label>E-mail:</label><br><input type="text" name="email" />
+        <button id="knop" onclick="alertEmail()">Versturen</button>
+        </article>
+
+        </footer>
+
+<script>
+function alertEmail() {
+    alert("Email is verzonden");
+}
+</script>
 </body>
-</html>`
+</html>
